06d8723147fef8c78f39308ed395564417be94f0
Marlon Enrique Noguera Ramirez <menoguerar@unal.edu.co>
2018-03-12 20:55:05 +0000

    Creacion de modelos Proyecto

---
+++ db/migrate/20180312204536_create_bicycles.rb
@@ -0,0 +1,15 @@
+class CreateBicycles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :bicycles do |t|
+      t.integer :id_bicy
+      t.string :brand_bicy
+      t.string :material_bicy
+      t.string :components_bicy
+      t.float :price_bicy
+      t.string :usetype_bicy
+      t.string :description_bicy
+
+      t.timestamps
+    end
+  end
+end
