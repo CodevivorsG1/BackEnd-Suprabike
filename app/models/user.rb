4b49aa7a54f539c2e0f86c5a846fb3cbeb1b39f9
Nicolas Enciso <nricardoe@unal.edu.co>
2018-03-15 12:13:06 -0500

    add relation city and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     validates :name : true
     has_many :transactions
+    belongs_to :city
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315170721) do
+ActiveRecord::Schema.define(version: 20180315171131) do
 
   create_table "bycicles", force: :cascade do |t|
     t.string "brand"
