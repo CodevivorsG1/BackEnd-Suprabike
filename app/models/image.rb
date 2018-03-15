d2964cb066b0f7efd5601d2f0193ab3068a87bdc
Nicolas Enciso <nricardoe@unal.edu.co>
2018-03-15 00:56:38 -0500

    relation 1 to n technician to transaction

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,2 @@
 class Image < ApplicationRecord
-<<<<<<< Updated upstream
-=======
-    belongs_to :imageable , polymorphic: true
->>>>>>> Stashed changes
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,6 @@ class Transaction < ApplicationRecord
     validates :date_transaction, presence: true
     validates :type_transaction, presence: true,length: { maximum: 50 }
     validates :total_transaction, presence: true, numericality: { only_float: true }
+    #relations
+    belongs_to :technician
 end
