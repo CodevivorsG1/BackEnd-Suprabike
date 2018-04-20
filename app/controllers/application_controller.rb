2f2e5c941717b2f3956b202d7f7071987204387c
Nicolas Enciso <nricardoe@unal.edu.co>
2018-04-19 20:39:14 -0500

    anadido a app controller Mime para hacer respond_to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::API
     #def home
     #    @posts = Post.index #-> whatever you want here
     #end
+     
+    include ActionController::MimeResponds
+      
 end
