7ef105f9cab1c79d69cc7e8a2d346a6e662c3a19
Marlon Enrique Noguera Ramirez <menoguerar@unal.edu.co>
2018-04-25 09:03:35 -0500

    heroku

--- db/migrate/20180415205529_add_store_to_comp.rb
+++ db/migrate/20180415205529_add_store_to_comp.rb
@@ -1,7 +1,7 @@
 class AddStoreToComp < ActiveRecord::Migration[5.1]
-  def change
+=begin   def change
     add_column :components, :store_id, :integer
     add_index :components, :store_id
   end 
-
+=end
 end
