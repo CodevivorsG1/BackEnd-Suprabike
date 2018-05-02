1638c0061e247f28215ae63fb41e0a8666ce349e
Marlon Enrique Noguera Ramirez <menoguerar@unal.edu.co>
2018-05-01 21:50:04 -0500

    foto

--- db/migrate/20180501230354_add_image_to_users.rb
+++ db/migrate/20180501230354_add_image_to_users.rb
@@ -3,8 +3,8 @@ class AddImageToUsers < ActiveRecord::Migration[5.1]
     #add_column :images, :user_id, :integer
     #add_index :images, :user_id
 
-    add_column :images,:store_id, :integer
-    add_index :images,:store_id
+    #add_column :images,:store_id, :integer
+    #add_index :images,:store_id
 
     add_column :images,:technician_id, :integer
     add_index :images,:technician_id
