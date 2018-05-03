a90db62aae13fcf4802ccb20de4c3b2068d4687f
Nicolas Enciso <nricardoe@unal.edu.co>
2018-05-02 23:23:09 -0500

    eliminacion de conflicto mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,15 +30,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   #Change mail delivery to either :smtp, :sendmail, :file, :test
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
+  
 
   config.action_mailer.perform_caching = false
 
